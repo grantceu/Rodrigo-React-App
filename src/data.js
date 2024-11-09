43ca3a027928b1b3b974fa0d4424d6783bdf26c0
grantceu <rodrigo2103530@ceu.edu.ph>
2024-11-09 17:53:39 +0800

    initial upload

---
+++ src/data.js
@@ -0,0 +1,48 @@
+export const projects = [
+    {
+      title: "Resident Evil 4 Remake",
+      subtitle: "PS5",
+      description:
+        "Resident Evil 4 is a 2023 survival horror game developed and published by Capcom. A remake of the 2005 game Resident Evil 4, players control the US agent Leon S. Kennedy, who must save Ashley Graham, the daughter of the United States president, from the mysterious Los Illuminados cult.",
+      image: "./project-1.gif",
+      link: "https://www.playstation.com/en-ph/games/resident-evil-4-remake/",
+    },
+    {
+      title: "The Last of Us",
+      subtitle: "PS3",
+      description:
+        "The Last of Us is a 2013 action-adventure game developed by Naughty Dog and published by Sony Computer Entertainment. Players control Joel, a smuggler tasked with escorting a teenage girl, Ellie, across a post-apocalyptic United States. The Last of Us is played from a third-person perspective.",
+      image: "./project-2.gif",
+      link: "https://www.amazon.com/Last-Us-PlayStation-3-sony-playstation3/dp/B007CM0K86",
+    },
+    {
+      title: "Fears To Fanthom Franchise",
+      subtitle: "Steam",
+      description:
+        "Fears to Fathom is a series of psychological horror anthology games developed and published by indie video game developer Rayll. The series debuted on July 2, 2021. The series has five games in the first season.",
+      image: "./project-3.gif",
+      link: "https://store.steampowered.com/franchise/f2f",
+    },
+    {
+      title: "Fallout 4",
+      subtitle: "Ps4",
+      description:
+        "Fallout 4 is a 2015 action role-playing game developed by Bethesda Game Studios and published by Bethesda Softworks. It is the fourth main game in the Fallout series and was released worldwide on November 10, 2015, for PlayStation 4, Windows, and Xbox One.",
+      image: "./project-4.gif",
+      link: "https://store.steampowered.com/app/377160/Fallout_4/",
+    },
+  ];
+
+
+export const skills = [
+  "Reflex",
+  "6th Senses",
+  "Calm",
+  "Silent",
+  "Epic Gamer",
+  "Hacker",
+  "Sleep",
+  "Sleeper Build",
+  "Collaborative",
+  "Clout"
+];
